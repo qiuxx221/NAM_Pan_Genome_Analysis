9a49ac530450b3df7a8cacaf38d666a4cc6aad38
Yinjie Qiu <35151706+qiuxx221@users.noreply.github.com>
2020-12-08 10:20:41 -0600

    Update pangenome_freq_include_subgenome_plot.R

--- data_visualization/pangenome_freq_include_subgenome_plot.R
+++ data_visualization/pangenome_freq_include_subgenome_plot.R
@@ -1,14 +1,10 @@
-library(data.table)
-library(ggplot2)
-library(ggsci)
-library(table)
-# add path to input file
-pan_matrix <- read.csv("pan_freq_sub_plotting_matrix_fix.txt", sep = "\t")
+setwd("~/Desktop/pan_genome_nov 2/QC_set/subgenome_and_pan_frequency/")
+# read in collasped final matrix, add the pan gene as the firs column "pan_gene_##" 
+pan_gene__freq_matrix <- read.csv("subgenome_pan_gene_frequency_for_visualization.csv")
 
 
-
-gene_frequency_plot <- ggplot(pan_matrix, aes(x = genome_presence, y = count)) + 
-  geom_bar(aes(fill = pan_gene_type), position = "stack", stat = "identity") +
+gene_frequency_plot <- ggplot(pan_gene__freq_matrix, aes(x = genome_presence, y = count)) + 
+  geom_bar(aes(fill = pan_gene_type), position = "stack", stat = "identity",fill = "black") +
   geom_bar(aes(alpha = subgenome), stat = "identity", fill = "grey") +
   scale_alpha_manual(values = c(0.9, 0.5, 0)) + 
   labs(x = "Number of Genomes",
@@ -17,14 +13,17 @@ gene_frequency_plot <- ggplot(pan_matrix, aes(x = genome_presence, y = count)) +
        alpha = "Maize Subgenome") +
   scale_x_continuous(breaks = seq(1, 26, 1)) +
   ylim(0, 30000) + 
-  theme(text = element_text(size = 20),legend.key = element_rect(fill = "black")) +
-  scale_fill_npg() + 
-  theme_classic() + 
-  theme(legend.position = "none")
+  theme(text = element_text(size = 12),legend.key = element_rect(fill = "black")) + theme_classic()
+  
 
+gene_frequency_plot + theme(legend.position = "none") 
+ 
+  
 gene_frequency_plot + theme(legend.key=element_rect(color="black"))  
 
 ### add the piechart in the exisiting plot 
+#pan gene frequency 
+gene_freq_for_visualization <- read.csv("subgenome_pan_gene_frequency_for_visualization.csv")
 # piechart version 
 blank_theme <- theme_minimal() + 
   theme(
@@ -43,8 +42,8 @@ bp<- ggplotGrob(ggplot(gene_freq_for_visualization, aes(x="genome_presence", y=P
 
 
 #ggplot(gene_freq_for_visualization, aes(x="genome_presence", y=Pan_gene, fill=class))+
- # geom_bar(width = 1, stat = "identity")  +scale_fill_npg() + 
-  coord_polar("y") + blank_theme + labs(fill = "Pan Gene Type")+ theme(legend.position ="none")
+# geom_bar(width = 1, stat = "identity")  +scale_fill_npg() + 
+coord_polar("y") + blank_theme + labs(fill = "Pan Gene Type")+ theme(legend.position ="none")
 
 #pdf("~/Desktop/NAM/gene_frequency_pie1.pdf", width=11,height=6,pointsize=12, paper='special')
 pie_chart_gene_frequency <- pie + blank_theme 
@@ -60,11 +59,15 @@ pan_gene_frequency_anchor <- gene_frequency_plot +
   ) 
 
 pan_gene_frequency_piechart <- 
-  pan_gene_frequency_anchor + annotate("text", x = 8, y = 18000, label = "Core Genes: 27.13%",size = 4.5) + annotate("text", x = 16, y = 26000, label = "Softcore Genes: 4.00%",size = 4.5) +
-  annotate("text", x = 14, y = 12000, label = "Dispensable Genes: 49.08%",size =4.5) + annotate("text", x = 20, y = 18000, label = "Private Genes: 19.79%",size = 4.5) + 
+  pan_gene_frequency_anchor + annotate("text", x = 8, y = 18000, label = "Core Genes: 26.96%",size = 4.5) + annotate("text", x = 16, y = 26000, label = "Near-Core Genes: 4.00%",size = 4.5) +
+  annotate("text", x = 14, y = 12000, label = "Dispensable Genes: 49.35%",size =4.5) + annotate("text", x = 20, y = 18000, label = "Private Genes: 19.69%",size = 4.5) + 
   stat_summary(fun.y = sum, aes(label = ..y.., group = genome_presence), geom = "text",vjust=0.5, size=4, angle = 90,hjust =-0.1 )
 
-pan_gene_frequency_piechart
-  
+pan_gene_frequency_piechart + theme(legend.position = "none") + theme(axis.title.y = element_text(size=12),
+                                                                      axis.title.x = element_text(size=12),
+                                                                      axis.text.y = element_text(size=12), 
+                                                                      axis.text.x = element_text(size=12),
+                                                                      legend.text = element_text(size=12),
+                                                                      legend.title=element_blank())
 
 
