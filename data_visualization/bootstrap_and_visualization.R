1fe637ae922d699b0b07096dd84a49f33cb75531
Yinjie Qiu <35151706+qiuxx221@users.noreply.github.com>
2020-12-09 14:05:42 -0600

    Update bootstrap_and_visualization.R

--- data_visualization/bootstrap_and_visualization.R
+++ data_visualization/bootstrap_and_visualization.R
@@ -62,28 +62,29 @@ library(reshape2)
 library(ggsci)
 # prepare dataset 
 # bootstrap_stepwise
-setwd("~/Desktop/NAM_pan_genome_final/909090/bootstrap/")
+setwd("~/Desktop/pan_genome_nov 2/QC_set/bootstrap/")
 
 # Panel A Pan Gene 
 ##  use pan_gene_bs_plot_pcnt for figure grabbing 
 
 ## readin bootstrap pan gene data
-color_levels = brewer.pal(8,'YlGn')
-pan_gene_bs = read.table('', header=F)
+color_levels = brewer.pal(8,'Greys')
+pan_gene_bs = read.table('Dec_bootstrap_1000.txt', header=F)
 pan_gene_bs = data.frame(count=as.vector(t(pan_gene_bs)),Genome=rep(1:26, 1000))
 pan_gene_bs$Genome = as.factor(pan_gene_bs$Genome)
-Total_pan_genes = 102660
+Total_pan_genes = 103538
 
 pan_gene_bs_plot_pcnt = ggplot(pan_gene_bs, aes(x=Genome, y=count)) + 
   geom_rect(aes(xmin = -Inf, xmax = Inf, ymin = -Inf, ymax = Total_pan_genes*0.8),
-            fill = color_levels[2], alpha = 0.01) +
+            fill = color_levels[2]) +
   geom_rect(aes(xmin = -Inf, xmax = Inf, ymin = Total_pan_genes*0.8, ymax = Total_pan_genes*0.9),
-            fill = color_levels[3], alpha = 0.01) +
+            fill = color_levels[3]) +
   geom_rect(aes(xmin = -Inf, xmax = Inf, ymin = Total_pan_genes*0.9, ymax = Total_pan_genes*0.95),
-            fill = color_levels[4], alpha = 0.01) + 
+            fill = color_levels[4]) + 
   geom_rect(aes(xmin = -Inf, xmax = Inf, ymin = Total_pan_genes*0.95, ymax = Inf),
-            fill = color_levels[5], alpha = 0.01) + #these layers at the bottom
+            fill = color_levels[6]) + #these layers at the bottom
   geom_violin() + #this layer on the top
+  scale_y_continuous(limits=c(40000, 105000)) +
   scale_y_continuous(sec.axis = sec_axis(~ . *100/Total_pan_genes, breaks = c(50,60,70,80,85,90,95,100),name="Percentage of Pan Genes (%)")) +
   labs(x ="Number of Genomes", y = "Number of Pan Genes")  + 
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
@@ -99,4 +100,3 @@ require(scales)
 #pdf("pav_final.pdf", width=12,height=4,pointsize=12)
 pan_gene_bs_plot_pcnt + scale_y_continuous(labels = comma, sec.axis = sec_axis(~ . *100/Total_pan_genes, breaks = c(50,60,70,80,85,90,95,100),name="Percentage of Pan Genes (%)"))
 #dev.off()
-
